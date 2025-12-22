dc76d0c83c871f58cad88dc3c0941e12ecebe6bc
Aditya Gupta <realadityagupta2@gmail.com>
2025-12-22 16:23:54 +0530

    python environment conflicts solve

---
+++ packages/database/src/database/__init__.py
@@ -0,0 +1,3 @@
+from db.db import disconnectDB, connectDB, prisma
+
+__all__ = ["prisma", "disconnectDB", "connectDB"]
--- packages/db copy/index.ts
+++
@@ -1,3 +0,0 @@
-import { PrismaClient } from "./generated/prisma";
-
-export const primaClient = new PrismaClient()
